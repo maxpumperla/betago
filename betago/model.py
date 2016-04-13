4b787e8ed36bceaaa177b5cf5f3594be4269ecb4
Max Pumperla <max.pumperla@googlemail.com>
2016-04-13 16:15:09 +0200

    documentation for models

--- betago/model.py
+++ betago/model.py
@@ -6,16 +6,27 @@ from .dataloader.goboard import GoBoard
 
 
 class GoModel(object):
+    '''
+    GoModel is a simple Flask app served on localhost:5000, exposing a REST API to predict
+    go moves.
+    '''
 
     def __init__(self, model, processor):
+        '''
+        Parameters:
+        -----------
+        processor: Instance of betago.dataloader.processor.GoDataLoader, e.g. SevenPlaneProcessor
+        model: In principle this can be anything that can predict go moves, given data provided by the above
+               processor. In practice it may very well be (an extension of) a keras model plus glue code.
+        '''
         self.model = model
         self.processor = processor
         self.go_board = GoBoard(19)
-        self.num_planes = processor.num_planes
 
         self.start_service()
 
     def predict(self):
+        ''' Predict the next move '''
         return NotImplemented
 
     def start_server(self):
@@ -41,19 +52,26 @@ class GoModel(object):
         @app.route('/prediction', methods=['GET', 'POST'])
         @cross_origin()
         def next_move():
-            ''' predict next move and send to client '''
+            '''
+            Predict next move and send to client. Just a wrapper around the abstract predict method.
+            '''
             val = self.predict()
-            print(val)
             return val
 
         self.app.run(host='0.0.0.0', debug=True, threaded=True, use_reloader=False)
 
 
 class KerasBot(GoModel):
+    '''
+    KerasBot takes top_n predictions of a keras model and tries to apply the best move. If that move is illegal,
+    choose the next best, until the list is exhausted. If no more moves are left to play, continue with random
+    moves until a legal move is found.
+    '''
 
     def __init__(self, model, processor, top_n=10):
         super(KerasBot, self).__init__(model=model, processor=processor)
         self.top_n = top_n
+        self.num_planes = processor.num_planes
 
     def predict(self):
         content = request.json
@@ -103,7 +121,10 @@ class KerasBot(GoModel):
 
 
 class IdiotBot(GoModel):
-    def __init__(self, model, processor):
+    '''
+    Play random moves, like a good 30k bot.
+    '''
+    def __init__(self, model=None, processor=None):
         super(IdiotBot, self).__init__(model=model, processor=processor)
 
     def predict(self):
