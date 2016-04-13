1dc9be1fb7e9eeb419b6f01d1746c2f6e361f799
Max Pumperla <max.pumperla@googlemail.com>
2016-04-13 16:03:00 +0200

    Random bot for fun

---
+++ examples/run_idiot_bot.py
@@ -0,0 +1,10 @@
+import os
+import webbrowser
+from betago.model import IdiotBot
+
+# Open web frontend
+path = os.getcwd().replace('/examples', '')
+webbrowser.open('file://' + path + '/ui/demoBot.html', new=2)
+
+# Create an idiot bot, creating random moves and serve it.
+go_model = IdiotBot()
