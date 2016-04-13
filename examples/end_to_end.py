03560614f06862c7627a69e1c712036dc9b145d8
Max Pumperla <max.pumperla@googlemail.com>
2016-04-13 18:44:26 +0200

    docs

--- examples/end_to_end.py
+++ examples/end_to_end.py
@@ -21,7 +21,7 @@ nb_conv = 3  # convolution kernel size
 processor = SevenPlaneProcessor()
 input_channels = processor.num_planes
 
-# Load go data and one-hot encode labels
+# Load go data from 1000 KGS games and one-hot encode labels
 X, y = processor.load_go_data(num_samples=1000)
 X = X.astype('float32')
 Y = np_utils.to_categorical(y, nb_classes)
