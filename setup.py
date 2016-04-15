ffbeb926cfbadc87993cfc9c8896468078b5284b
Max Pumperla <max.pumperla@googlemail.com>
2016-04-15 08:57:02 +0200

    prepare release 0.0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='betago',
-      version='0.0.1',
+      version='0.0.2',
       description='Create your own go bot from scratch. We are Lee Sedol!',
       url='http://github.com/maxpumperla/betago',
-      download_url='https://github.com/maxpumperla/betago/tarball/0.0.1',
+      download_url='https://github.com/maxpumperla/betago/tarball/0.0.2',
       author='Max Pumperla',
       author_email='max.pumperla@googlemail.com',
-      install_requires=['keras', 'gomill'],
+      install_requires=['keras', 'gomill', 'flask', 'flask-cors', 'future'],
       license='MIT',
       packages=find_packages(),
       zip_safe=False)
