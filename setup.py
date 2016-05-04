7e7802454d9a06093ae23eb8947606b40e3e0aaa
Kevin Ferguson <kevin.a.ferguson@gmail.com>
2016-05-04 07:23:44 -0700

    Add six to deps in setup.py.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='betago',
       download_url='https://github.com/maxpumperla/betago/tarball/0.2',
       author='Max Pumperla',
       author_email='max.pumperla@googlemail.com',
-      install_requires=['keras', 'gomill', 'Flask>=0.10.1', 'Flask-Cors', 'future', 'h5py'],
+      install_requires=['keras', 'gomill', 'Flask>=0.10.1', 'Flask-Cors', 'future', 'h5py', 'six'],
       license='MIT',
       packages=find_packages(),
       zip_safe=False)
