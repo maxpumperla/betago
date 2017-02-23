a75949ea36c11494ad8b0e4d7ec785cad21d7d6f
Elliot Gerchak <e@dementedlab.com>
2017-02-23 19:00:40 +0000

    Use port 8081 instead 0f 5000

--- betago/model.py
+++ betago/model.py
@@ -57,7 +57,7 @@ class HTTPFrontend(object):
             json_result = jsonify(**result)
             return json_result
 
-        self.app.run(host='0.0.0.0', debug=True, threaded=True, use_reloader=False)
+        self.app.run(host='0.0.0.0', port=int("8081"), debug=True, threaded=True, use_reloader=False)
 
 
 class GoModel(object):
